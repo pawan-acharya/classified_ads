c66d6b738568f9ff0282e9c093ace2b14de10d34
pawan-acharya <pawan@ux-qode.com>
2020-08-12 15:09:35 +0545

    Revert "--> change in edit section for undifed offset (array)"
    This reverts commit 3056fcb6b64f8ce038cac4e9c57f164b67063bd6.

--- resources/views/classified_ads/partials/edit.blade.php
+++ resources/views/classified_ads/partials/edit.blade.php
@@ -3,21 +3,15 @@
 	    <label for="exampleInputEmail1">{{$form_item->name}}</label>
 		@switch($form_item->type)
 		    @case('text')
-		        <input type="text" class="form-control" name="{{$form_item->id}}-{{$form_item->name}}" 
-		        value="{{array_key_exists( $form_item->id, json_decode($classified_ad->form_values, true)) ? json_decode($classified_ad->form_values, true)[$form_item->id]: ''}}" 
-		        {{($form_item->required )?'required':''}}>
+		        <input type="text" class="form-control" name="{{$form_item->id}}-{{$form_item->name}}" value="{{json_decode($classified_ad->form_values, true)[$form_item->id]}}" aria-describedby="emailHelp" {{($form_item->required )?'required':''}}>
 		    	<small id="emailHelp" class="form-text text-muted">&Lorem ipsum dolor sit amet</small>
 		        @break
 	        @case('number')
-		       	<input type="number" class="form-control" name="{{$form_item->id}}-{{$form_item->name}}" 
-		       	value="{{array_key_exists( $form_item->id, json_decode($classified_ad->form_values, true)) ? json_decode($classified_ad->form_values, true)[$form_item->id]: ''}}"  
-		       	{{($form_item->required )?'required':''}}>
+		       	<input type="number" class="form-control" name="{{$form_item->id}}-{{$form_item->name}}" value="{{json_decode($classified_ad->form_values, true)[$form_item->id]}}" aria-describedby="emailHelp"  {{($form_item->required )?'required':''}}>
 		    	<small id="emailHelp" class="form-text text-muted">&Lorem ipsum dolor sit amet</small>
 		        @break
 	        @case('date')
-		        <input type="date" class="form-control" name="{{$form_item->id}}-{{$form_item->name}}" 
-		        value="{{array_key_exists( $form_item->id, json_decode($classified_ad->form_values, true)) ? json_decode($classified_ad->form_values, true)[$form_item->id]: ''}}" 
-		        {{($form_item->required )?'required':''}}>
+		        <input type="date" class="form-control" name="{{$form_item->id}}-{{$form_item->name}}" value="{{json_decode($classified_ad->form_values, true)[$form_item->id]}}" aria-describedby="emailHelp"  {{($form_item->required )?'required':''}}>
 		    	<small id="emailHelp" class="form-text text-muted">&Lorem ipsum dolor sit amet</small>
 		        @break
 		    @default
